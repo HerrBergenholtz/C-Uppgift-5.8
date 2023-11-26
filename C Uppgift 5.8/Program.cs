b0de7d07261958d175b35befe5ece24ba9b40962
HerrBergenholtz <zbdv8800@gmail.com>
2023-11-26 19:38:56 +0100

    Add project files.

---
+++ C Uppgift 5.8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static async Task Main()
+    {
+        int[] talLista = { 1234, 2345, 3456, 4567, 5678 };
+        await Delay(talLista);
+        Console.WriteLine("Programmet har slutförts");
+        Console.ReadKey();
+    }
+    public static async Task Delay(int[] talLista)
+    {
+        foreach (int tal in talLista)
+        {
+            await Task.Delay(1000);
+            string talSträng = tal.ToString();
+
+            foreach (char siffra in talSträng)
+            {
+                Console.Write(siffra);
+                await Task.Delay(500);
+            }
+            Console.WriteLine();
+        }
+    }
+}
